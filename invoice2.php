6a83bc097c30d13f446ce3caf27c330245df2566
Surya mishra <144292753+Suryanodejs@users.noreply.github.com>
2023-09-25 12:19:55 +0530

    Add files via upload

---
+++ invoice2.php
@@ -0,0 +1,359 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Visa Application</title>
+    <link rel="" href="css/bootstrap.min.css">
+
+    <style>
+    .p {
+        margin: 0;
+        padding: 0;
+    }
+    </style>
+</head>
+
+<body>
+
+
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="">
+                    <h5 class=""></h5>
+                    <div class="card-body">
+
+                        <!-- new  -->
+
+
+                    </div>
+
+                    <div class="card-body" style="
+                    max-width: 100px;
+                    max-height: 374px;
+                    /* margin: 200px; */
+                     "></div>
+
+
+                    <div class="card-body">
+                        <!-- new  -->
+                        <table class="table table-bordered">
+
+
+
+                            <thead>
+
+                                <tr>
+                                    <th colspan="15" style="">
+                                        <p style="margin: 0;padding: 0; text-align: center;">Tax INVOICE</p>
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <thead>
+
+                                <tr>
+                                    <th colspan="7" style="">
+                                        <p style="margin: 0;padding: 0; text-align: center;">Bill No. NFCPL/23-24/1435
+                                        </p>
+
+                                    </th>
+                                    <th colspan="8" style="">
+                                        <p style="margin: 0;padding: 0; text-align: center;">Date: - 01.06.2023 </p>
+
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <thead>
+
+                                <tr>
+                                    <th colspan="15" style="">
+                                        <p style="margin: 0;padding: 0;">M/S TATA MOTORS BODY SOLUTIONS LTD</p>
+                                        <p style="margin: 0;padding: 0;">CHINHAT INDUSTRIAL AREA </p>
+                                        <p style="margin: 0;padding: 0;">DEWA ROAD, LUCKNOW </p>
+                                        <p style="margin: 0;padding: 0;">UP-226019</p>
+                                    </th>
+
+
+                                </tr>
+                            </thead>
+
+                            <thead>
+
+                                <tr>
+                                    <th colspan="15" style="">
+                                        <p style="margin: 0;padding: 0;">GSTIN NO-09AACCT5547J1ZY</p>
+
+                                    </th>
+
+
+                                </tr>
+                            </thead>
+
+
+                            <tbody>
+
+                                <tr>
+                                    <th colspan="1">Sl.No</th>
+                                    <th colspan="1">Bkg.Stn </th>
+                                    <th colspan="1">AWBNo</th>
+                                    <th colspan="1">Date </th>
+                                    <th colspan="1"> DLY.Stn</th>
+                                    <th colspan="1">pkt </th>
+                                    <th colspan="1"> Wt </th>
+                                    <th colspan="1"> Mode</th>
+                                    <th>Rate</th>
+                                    <th> Freight.ch </th>
+                                    <th> AWB.ch</th>
+                                    <th>Pick-up</th>
+                                    <th>Delivery</th>
+                                    <th>Spe. Del</th>
+                                    <th>Sub Total</th>
+
+
+                                </tr>
+                                <tr>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1"> 26
+                                    </td>
+                                    <td colspan="1">4076
+                                    </td>
+                                    <td colspan="1">48.5
+                                    </td>
+                                    <td colspan="1">86510
+                                    </td>
+                                    <td colspan="1"> 30413
+                                    </td>
+                                    <td colspan="1">0
+                                    </td>
+                                    <td colspan="1"></td>
+                                    <td colspan="1"> 0</td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1"> 26
+                                    </td>
+                                    <td colspan="1">4076
+                                    </td>
+                                    <td colspan="1">48.5
+                                    </td>
+                                    <td colspan="1">86510
+                                    </td>
+                                    <td colspan="1"> 30413
+                                    </td>
+                                    <td colspan="1">0
+                                    </td>
+                                    <td colspan="1"></td>
+                                    <td colspan="1"> 0</td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1"> 26
+                                    </td>
+                                    <td colspan="1">4076
+                                    </td>
+                                    <td colspan="1">48.5
+                                    </td>
+                                    <td colspan="1">86510
+                                    </td>
+                                    <td colspan="1"> 30413
+                                    </td>
+                                    <td colspan="1">0
+                                    </td>
+                                    <td colspan="1"></td>
+                                    <td colspan="1"> 0</td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1">1
+                                    </td>
+                                    <td colspan="1">30/06/2023
+                                    </td>
+                                    <td colspan="1">MH46BB4690
+                                    </td>
+                                    <td colspan="1"> 26
+                                    </td>
+                                    <td colspan="1">4076
+                                    </td>
+                                    <td colspan="1">48.5
+                                    </td>
+                                    <td colspan="1">86510
+                                    </td>
+                                    <td colspan="1"> 30413
+                                    </td>
+                                    <td colspan="1">0
+                                    </td>
+                                    <td colspan="1"></td>
+                                    <td colspan="1"> 0</td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="2">Total
+                                    </td>
+                                    <!-- <td colspan="1"></td> -->
+                                    <td colspan="1"> </td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="2">IGST @ 18%
+                                    </td>
+                                    <!-- <td colspan="1"></td> -->
+                                    <td colspan="1"> </td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="1">
+                                    </td>
+                                    <td colspan="2">Grand Total
+                                    </td>
+                                    <!-- <td colspan="1"></td> -->
+                                    <td colspan="1"> </td>
+                                    <td colspan="1">116923
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
